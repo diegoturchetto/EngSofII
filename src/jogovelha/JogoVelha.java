549bd5df286331e4c090c98c2486fc8fac7752cf
lfilipini <57815285+lfilipini@users.noreply.github.com>
2019-11-20 21:19:18 -0300

    Update JogoVelha.java

--- src/jogovelha/JogoVelha.java
+++ src/jogovelha/JogoVelha.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jogovelha;
 
 import javafx.application.Application;
@@ -20,7 +16,7 @@ import javafx.scene.layout.*;
 
 /**
  *
- * @author diego e luciano
+ * @author diego e Luciano
  */
 public class JogoVelha extends Application {
 
